41cf807b31bd7c4ac69860b058225b238aba1a9c
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-02-03 03:00:19 +0300

    feat: add SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { parse } from 'yaml';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
+import { PORT } from './environments';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+    const document = await readFile(
+      join(__dirname, '..', 'doc/api.yaml'),
+      'utf-8',
+    );
+
+    SwaggerModule.setup('doc', app, parse(document));
+
+    await app.listen(PORT, () =>
+      Logger.log(`🚀  Server ready at https://localhost:${PORT}`, 'Bootstrap'),
+    );
+  } catch (error) {
+    Logger.error(`❌  Error starting server, ${error}`, '', 'Bootstrap', false);
+    process.exit(1);
+  }
 }
 bootstrap();
