271a591381dedf37bf12787877de95d877b3de68
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-02-04 01:14:08 +0300

    refactor: rename folder albums

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/users/users.module';
-import { AlbumModule } from './modules/album/albums.module';
+import { AlbumModule } from './modules/albums/albums.module';
 
 @Module({
   imports: [UserModule, AlbumModule],
--- src/db/in-memory.db.ts
+++ src/db/in-memory.db.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 import { UserModel } from '../modules/users/user.model';
-import { AlbumModel } from '../modules/album/album.model';
+import { AlbumModel } from '../modules/albums/album.model';
 
 @Injectable()
 export class InMemoryDb {
