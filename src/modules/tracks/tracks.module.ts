d4ab70e93d147ceb6d1e02b6dc8aa85deb2c4929
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-02-18 19:42:18 +0300

    feat: add typeorm in tracks

--- src/modules/tracks/tracks.module.ts
+++ src/modules/tracks/tracks.module.ts
@@ -1,13 +1,18 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { forwardRef, Module } from '@nestjs/common';
 
 import { TrackService } from './tracks.service';
 import { TrackController } from './tracks.controller';
-import { DbModule } from '../../db/db.module';
+
+import { TrackEntity } from './track.entity';
 
 import { FavoritesModule } from '../favorites/favorites.module';
 
 @Module({
-  imports: [DbModule, forwardRef(() => FavoritesModule)],
+  imports: [
+    TypeOrmModule.forFeature([TrackEntity]),
+    forwardRef(() => FavoritesModule),
+  ],
   exports: [TrackService],
   controllers: [TrackController],
   providers: [TrackService],
