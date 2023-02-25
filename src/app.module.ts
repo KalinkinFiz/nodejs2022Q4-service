fdd3f74d48ed7e51c27ecf2ad7b1f2b4c771b6e7
KalinkinFiz <kalinkin.fiz@gmail.com>
2023-02-25 21:37:02 +0300

    feat: add logger in main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,6 +14,10 @@ import { AuthModule } from './auth/auth.module';
 
 import { AppDataSource } from './config.orm';
 
+import { LoggerModule } from './common/logger.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot(AppDataSource.options),
@@ -22,8 +27,19 @@ import { AppDataSource } from './config.orm';
     TrackModule,
     FavoritesModule,
     AuthModule,
+    LoggerModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('/');
+  }
+}
